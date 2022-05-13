6d15eb45aa6b7b30193051238543781d843e646a
Momchil Marinov <mom4ilakis@gmail.com>
2022-05-13 19:50:44 +0300

    Version API

--- v1/main.py
+++ v1/main.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,7 +6,7 @@ from disk_usage import DiskUsageView
 
 app = FastAPI()
 
-app.mount('/static', StaticFiles(directory='./static'), name='static')
+app.mount('static', StaticFiles(directory='./static'), name='static')
 
 
 @app.get("/", response_class=HTMLResponse)
