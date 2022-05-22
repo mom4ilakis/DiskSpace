4cc5f13261dcae40922e91d91f8972ceded948ad
Momchil Marinov <mom4ilakis@gmail.com>
2022-05-22 11:24:36 +0300

    Remove unused code
    Signed-off-by: Momchil Marinov <mom4ilakis@gmail.com>

--- v2/server/disk_space.py
+++ v2/server/disk_space.py
@@ -28,24 +28,3 @@ def get_space(path_to_scan='', space_type=SpaceType.TOTAL, unit='KB'):
     space_info = shutil.disk_usage(path_to_scan)
 
     return format_size(getattr(space_info, space_type.value), MemoryUnits[unit])
-
-
-class DiskSpace(object):
-    def __init__(self, path, to_format=MemoryUnits.KB):
-        self.path = path
-        self.format = to_format
-
-    def get_free_space(self):
-        [_, _, free] = shutil.disk_usage(self.path)
-
-        return format_size(free, self.format)
-
-    def get_total_space(self):
-        [total, _, _] = shutil.disk_usage(self.path)
-
-        return format_size(total, self.format)
-
-    def get_used_space(self):
-        [_, used, _] = shutil.disk_usage(self.path)
-
-        return format_size(used, self.format)
