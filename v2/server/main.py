60fda2b41f18de845a61397ea47f1ee0088dc693
Momchil <mom4ilakis@gmail.com>
2022-06-27 19:37:33 +0300

    Update origins

--- v2/server/main.py
+++ v2/server/main.py
@@ -1,5 +1,7 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 from strawberry.fastapi import GraphQLRouter
 
 from straw_berry import schema
@@ -8,18 +10,21 @@ from validations import ALLOWED_PATHS
 graphql_app = GraphQLRouter(schema)
 
 ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://192.168.0.158:3000',
+    'http://my.media.server:3000'
 ]
 
 app = FastAPI()
+
+app.include_router(graphql_app, prefix='/graphql')
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_methods=['*'],
     allow_headers=['*']
 )
-
-app.include_router(graphql_app, prefix='/graphql')
 
 
 @app.get('/')
@@ -30,3 +35,7 @@ async def root():
 @app.get('/allowed_paths')
 async def get_allowed_paths():
     return list(ALLOWED_PATHS)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8081)
